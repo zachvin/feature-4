5b7a3ed35a2b97908893e369ca6dae752a4642d7
Zach <zvincent@nd.edu>
2025-03-05 11:31:16 -0500

    family database push

---
+++ src/Components/Services/family.jsx
@@ -0,0 +1,19 @@
+import Parse from "parse";
+
+// Get a family with a specific ID
+export const getFamilyById = (id) => {
+  const Family = Parse.Object.extend("family");
+  const query = new Parse.Query(Family);
+  return query.find(id).then((result) => {
+    return result;
+  });
+};
+
+// Get all familys regardless of ID
+export const getAllFamilies = () => {
+  const Family = Parse.Object.extend("family");
+  const query = new Parse.Query(Family);
+  return query.find().then((results) => {
+    return results;
+  });
+};
