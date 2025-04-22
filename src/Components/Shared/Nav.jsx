3540e0c915708fe43f19d5770d2365632c02080a
Zach <zvincent@nd.edu>
2025-04-22 17:45:03 -0400

    random nav update I guess

--- src/Components/Shared/Nav.jsx
+++ src/Components/Shared/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="w-screen flex flex-wrap items-center align-middle justify-between p-4">
-        <h1 className="block text-2xl font-extrabold w-xs">john suxify</h1>
+        <h1 className="block text-2xl font-extrabold w-xs">website name</h1>
         <div
           className="items-center justify-between w-full md:flex md:w-auto"
           id="navbar-sticky"
